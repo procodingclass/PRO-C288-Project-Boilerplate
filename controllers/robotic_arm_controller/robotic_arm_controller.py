516df70b3f70cf1932ba35c9f35e2346efe81578
procodingclass <88050205+procodingclass@users.noreply.github.com>
2022-08-29 17:22:12 +0530

    Update robotic_arm_controller.py

--- controllers/robotic_arm_controller/robotic_arm_controller.py
+++ controllers/robotic_arm_controller/robotic_arm_controller.py
@@ -5,24 +5,15 @@ robot = Robot()
 kb = Keyboard()
 
 # create unique identifier for each device
-motorA = robot.getDevice("A motor")
+
 
 timestep = int(robot.getBasicTimeStep())
 kb.enable(timestep)
-
-motorA_pos = 0
 
 
 # - perform simulation steps until Webots is stopping the controller
 while robot.step(timestep) != -1:
     key_pressed = kb.getKey()   
     print(key_pressed) 
-    
-    # here's is an example of keyboard movement for A motor
-    if(key_pressed == 49):
-        motorA_pos += 0.01
-    if(key_pressed == 50):
-        motorA_pos -= 0.01
-        
-   
-    motorA.setPosition(motorA_pos)
+
+    # write code to move the joints with keyboard
